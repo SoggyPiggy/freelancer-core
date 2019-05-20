a8e0808e877765a6bc438655da24b7be238d821c
SoggyPiggy <soggypiggy@gmail.com>
2019-05-20 01:38:06 -0400

    Added some basic function overrides for the Base

--- src/Base.js
+++ src/Base.js
@@ -4,6 +4,24 @@ class Base {
     this._.keys = Object.keys(options);
     Object.assign(this, options);
   }
+
+  compress() {
+    const data = {};
+    this._.keys.forEach((key) => {
+      const item = this[key];
+      if (item instanceof Base) data[key] = item.compress();
+      else data[key] = item;
+    });
+    return data;
+  }
+
+  toJSON(...args) {
+    return JSON.stringify(this.compress(), ...args);
+  }
+
+  toString() {
+    return JSON.stringify(this.compress, null, 2);
+  }
 }
 
 module.exports = Base;
