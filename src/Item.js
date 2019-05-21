c7069fe386ab26ac372d2206b325060c0eca57ee
SoggyPiggy <soggypiggy@gmail.com>
2019-05-21 15:05:21 -0400

    Grouped and added new properties

--- src/Item.js
+++ src/Item.js
@@ -1,13 +1,26 @@
 const Base = require('./Base');
+const Requirements = require('./Requirements');
 
 class Item extends Base {
   constructor(data = {}) {
     super();
+    // Internal Values
     this.id = this._.property(data.id, null);
+
+    // Basic Information
     this.name = this._.property(data.name, null);
     this.examine = this._.property(data.examine, null);
-    this.value = this._.property(data.value, null);
+    this.value = this._.property(data.value, 0);
+
+    // Armor / Stat Boosts
     this.slot = this._.property(data.slot, null);
+    this.armor = this._.property(data.armor, null);
+    this.damage = this._.property(data.damage, null);
+    this.speed = this._.property(data.speed, null);
+
+    // Other
+    this.recipes = this._.property(data.recipes, [], Requirements, true);
+    this.equip = this._.property(data.equip, {}, Requirements);
   }
 }
 
